b53298ffb3f01176fb5f909c03fcbc1c96236635
Axe10rellana <axelorellana550@gmail.com>
2022-08-16 13:14:27 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Bank Modern App
+
+## Descripción
+
+Codigo Base De La Interfaz Del Proyecto Bank Modern App Hecha En React.js Y Tailwindcss En Fase De Desarrollo.
+
+## Comandos
+
+- npm run dev
+- npm run build
+- npm run preview
+
+## Dependencias
+
+## Dependencias De Desarrollo
+
+- autoprefixer
+- postcss
+- tailwindcss
---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+//importaciones
+import React from 'react';
+
+//estilos css
+import styles from "./style";
+
+//componentes
+import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from "./components";
+import BtnScrollTop from './components/BtnScrollTop';
+
+const App = () => {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <BtnScrollTop />
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
