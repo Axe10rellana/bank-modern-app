f4bdb9c0ef8df08eb2cb626cc799d6397645e199
Axe10rellana <axelorellana550@gmail.com>
2022-08-16 14:31:00 -0300

    updated project

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,10 @@ import styles from "./style";
 
 //componentes
 import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from "./components";
-import BtnScrollTop from './components/BtnScrollTop';
 
 const App = () => {
   return (
     <div className="bg-primary w-full overflow-hidden">
-      <BtnScrollTop />
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
           <Navbar />
